e1e50bfca34ad3e000c5627632c8f6b56271870c
Christopher Leggett <chris@leggett.dev>
2022-03-29 02:06:40 +0000

    📸 Adds image to home page 📸

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import * as React from "react";
 import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 // markup
 const IndexPage = () => {
     return (
         <Layout pageTitle="Home Page">
             <p>I'm making this by following the Gatsby Tutorial.</p>
+            <StaticImage alt="A picture of me, Christopher Leggett" src="../images/me.webp" className="rounded-full w-1/2" />
         </Layout>
     )
 }
