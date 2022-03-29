8becd0b748b1f4ecf7d44ca8fd3a0b70334dad37
Christopher Leggett <chris@leggett.dev>
2022-03-29 05:31:24 +0000

    🏗️Partial config of getting project pages working 🏗️

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,32 +1,32 @@
 import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
-  siteMetadata: {
-    title: `Christopher Leggett's Portfolio and Blog`,
-    description: `The Portfolio and Blog for the aspiring software developer Christopher Leggett`,
-    siteUrl: `https://chrisleggett.me`,
-    twitterUsername: `@leggettc18`
-  },
-  plugins: ["gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-postcss", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+    siteMetadata: {
+        title: `Christopher Leggett's Portfolio and Blog`,
+        description: `The Portfolio and Blog for the aspiring software developer Christopher Leggett`,
+        siteUrl: `https://chrisleggett.me`,
+        twitterUsername: `@leggettc18`
     },
-    __key: "images"
-  }, {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
-    },
-    __key: "pages"
-  }]
+    plugins: ["gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-postcss", {
+        resolve: 'gatsby-plugin-manifest',
+        options: {
+            "icon": "src/images/icon.png"
+        }
+    }, "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                "name": "images",
+                "path": "./src/images/"
+            },
+            __key: "images"
+        }, {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                "name": "pages",
+                "path": "./src/pages/"
+            },
+            __key: "pages"
+        }]
 };
 
 export default config;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,22 @@
 import * as React from "react";
 import Layout from "../components/layout";
 import { StaticImage } from "gatsby-plugin-image";
+import { useStaticQuery, graphql } from "gatsby";
 
 // markup
 const IndexPage = () => {
+    const data = useStaticQuery(graphql`
+        query{
+            site {
+                siteMetadata {
+                    description
+                }
+            }
+        }
+    `)
     return (
         <Layout pageTitle="Christopher Leggett">
-            <p>Portfolio and Blog of East Carolina University Graduate and Aspiring Software Developer</p>
+            <p>{data.site.siteMetadata.description}</p>
             <div className="flex justify-center">
                 <StaticImage alt="A picture of me, Christopher Leggett" src="../images/me.webp" className="rounded-full w-52 self-center" />
             </div>
