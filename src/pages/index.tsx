17677212ad2e4a1598166f89f3cd1ff7db32686d
Christopher Leggett <chris@leggett.dev>
2022-03-30 03:32:26 +0000

    🔼 Improves layout of home page 🔼

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,9 +16,14 @@ const IndexPage = () => {
     `)
     return (
         <Layout pageTitle="Christopher Leggett">
-            <p>{data.site.siteMetadata.description}</p>
-            <div className="flex justify-center">
-                <StaticImage alt="A picture of me, Christopher Leggett" src="../images/me.webp" className="rounded-full w-52 self-center" />
+            <div className="flex flex-col md:flex-row items-center">
+                <div>
+                    <p>{data.site.siteMetadata.description}</p>
+                    <p>Welcome to the redesigned portfolio! The styling is still a bit basic compared to the old Hugo site but that will be fixed in time.</p>
+                </div>
+                <div className="flex justify-center">
+                    <StaticImage alt="A picture of me, Christopher Leggett" src="../images/me.webp" className="rounded-full w-52 md:w-72 self-center m-4" />
+                </div>
             </div>
         </Layout>
     )
