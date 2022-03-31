642de40ed9458f1e71e42dd8ce981e1bfa8511a0
Christopher Leggett <chris@leggett.dev>
2022-03-31 03:11:52 +0000

    💥Adds featured section to Home page💥

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -23,13 +23,13 @@ const BlogPage = ({ data }: PageProps<DataProps>) => {
                 data.allMdx.nodes.map((node) => (
                     <article key={node.id} className="dark:bg-zinc-800 bg-zinc-200 p-4 rounded">
                         <h2 className='m-0'>
-                            <Link className="text-zinc-700 dark:text-zinc-300 hover:text-purple-600 dark:hover:text-purple-300 transition duration-200 no-underline" to={`/blog/${node.slug}`}>
+                            <Link className="text-zinc-700 dark:text-zinc-300 hover:text-purple-600 dark:hover:text-purple-400 transition duration-200 no-underline" to={`/blog/${node.slug}`}>
                                 {node.frontmatter.title}
                             </Link>
                         </h2>
                         <p>Posted: {node.frontmatter.date}</p>
                         <p>{node.excerpt}</p>
-                        <Link className='dark:hover:text-purple-300 hover:text-purple-600 transition duration-200' to={`/blog/${node.slug}`}>Read more...</Link>
+                        <Link className='dark:hover:text-purple-400 hover:text-purple-600 transition duration-200' to={`/blog/${node.slug}`}>Read more...</Link>
                     </article>
                 ))
             }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,26 @@
 import * as React from "react";
 import Layout from "../components/layout";
 import { StaticImage } from "gatsby-plugin-image";
-import { useStaticQuery, graphql } from "gatsby";
+import { useStaticQuery, graphql, Link } from "gatsby";
 
 // markup
 const IndexPage = () => {
-    const data = useStaticQuery(graphql`
-        query{
+    const data: any = useStaticQuery(graphql`
+        query {
             site {
                 siteMetadata {
                     description
+                }
+            }
+            allMdx(sort: {fields: frontmatter___portfolioScore, order: DESC }, filter: { fields: { source: { eq: "portfolio" }}, frontmatter: { featured: { eq: true }}}) {
+                nodes {
+                    frontmatter {
+                        date(formatString: "MMMM D, YYYY")
+                        title
+                        portfolioScore
+                    }
+                    slug
+                    id
                 }
             }
         }
@@ -24,6 +35,20 @@ const IndexPage = () => {
                 <div className="flex justify-center">
                     <StaticImage alt="A picture of me, Christopher Leggett" src="../images/me.webp" className="rounded-full w-52 md:w-72 self-center m-4" />
                 </div>
+            </div>
+            <h2>Featured</h2>
+            <div className="flex flex-wrap justify-center">
+                {
+                    data.allMdx.nodes.map((node: any) => (
+                        <article key={node.id} className="dark:bg-zinc-800 rounded p-4 m-2 flex-1">
+                            <h3 className="m-0 text-center">
+                                <Link className="text-zinc-700 dark:text-zinc-300 hover:text-purple-600 dark:hover:text-purple-400 no-underline transition duration-200" to={`/portfolio/${node.slug}`}>
+                                    {node.frontmatter.title}
+                                </Link>
+                            </h3>
+                        </article>
+                    ))
+                }
             </div>
         </Layout>
     )
