a0ee6ba7c3987cc7ded2bba244a9d7f9c9feea09
Christopher Leggett <chris@leggett.dev>
2022-04-26 00:41:06 +0000

    🚧 Fixes some overlap issues on the smaller end of large screens 🚧

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -74,8 +74,8 @@ const Layout: FunctionComponent<LayoutProps> = ({ pageTitle, location, children 
                             </Link>
                         </ul>
                     </nav>
-                    <main className="flex justify-center w-full mt-5 md:m-9 rounded-xl mb-20">
-                        <div className="prose lg:prose-xl prose-zinc dark:prose-invert md:max-w-[65ch] lg:max-w-[80ch]  prose-h2:mt-2 lg:prose-h2:mt-2">
+                    <main className="flex justify-center w-full mt-5 rounded-xl mb-20">
+                        <div className="prose lg:prose-xl prose-zinc dark:prose-invert md:max-w-[65ch] lg:max-w-[75ch] xl:max-w-[90ch] prose-h2:mt-2 lg:prose-h2:mt-2">
                             <TransitionViews>
                                 {children}
                             </TransitionViews>
