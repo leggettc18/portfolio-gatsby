c82802fa872512255622c7a3d01d435214d2fd8b
Christopher Leggett <chris@leggett.dev>
2022-05-12 05:20:09 +0000

    Reimplements prose styles on about page.

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -4,14 +4,16 @@ import SEO from "../components/seo";
 
 const AboutPage = () => {
     return (
-        <>
-            <h1 className="px-9 md:px-4">About Me</h1>
-            <SEO title="About Me" />
-            <div className="px-8">
-                <p>My name is Christopher Leggett, I’m an East Carolina University Graduate with a Bachelor of Science Degree in Computer Science. I am currently an MSP IT Technician by trade but my end goal is to be a Full Stack or Backend Developer.</p>
-                <p>This Portfolio/Blog is written in Gatsby. It leverages Tailwind CSS for styling, dark-mode, and more, and PrismJS for Code Highlighting with the Tomorrow Night theme.</p>
+        <div className="flex justify-center">
+            <div className="prose lg:prose-xl prose-zinc dark:prose-invert md:max-w-[65ch] lg:max-w-[75ch] xl:max-w-[90ch] prose-h2:mt-2 lg:prose-h2:mt-2">
+                <h1 className="px-9 md:px-4">About Me</h1>
+                <SEO title="About Me" />
+                <div className="px-8">
+                    <p>My name is Christopher Leggett, I’m an East Carolina University Graduate with a Bachelor of Science Degree in Computer Science. I am currently an MSP IT Technician by trade but my end goal is to be a Full Stack or Backend Developer.</p>
+                    <p>This Portfolio/Blog is written in Gatsby. It leverages Tailwind CSS for styling, dark-mode, and more, and PrismJS for Code Highlighting with the Tomorrow Night theme.</p>
+                </div>
             </div>
-        </>
+        </div>
     )
 }
 
