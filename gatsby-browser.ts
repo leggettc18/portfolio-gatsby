3c606f6640c105487ee4f4173e6af1dd436d1d03
Christopher Leggett <chris@leggett.dev>
2023-07-03 02:23:21 -0400

    Fixes hydration error

--- gatsby-browser.ts
+++ gatsby-browser.ts
@@ -1,6 +1,13 @@
 import './src/css/index.css';
 import "prismjs/themes/prism-tomorrow.css";
+import ReactDOM from 'react-dom';
 
 export const shouldUpdateScroll = () => {
     return false;
+}
+
+export const replaceHydrateFunction = () => {
+    return (element, container) => {
+        return ReactDOM.render(element, container);
+    };
 }
