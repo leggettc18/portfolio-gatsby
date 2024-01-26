a5021dace13e117e3084f123482903637dc22444
Christopher Leggett <chris@leggett.dev>
2024-01-26 17:35:19 -0500

    Updates description

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,20 +1,25 @@
-import { GatsbyConfig } from 'gatsby';
-import path from 'path';
+import { GatsbyConfig } from "gatsby";
+import path from "path";
 
 const config: GatsbyConfig = {
     siteMetadata: {
         title: `Christopher Leggett's Portfolio and Blog`,
-        description: `The Portfolio and Blog for the aspiring software developer Christopher Leggett`,
+        description: `The Software Development Portfolio and Blog for Christopher Leggett`,
         url: `https://chrisleggett.me`,
-        image: '/me.png',
-        twitterUsername: `@leggettc18`
+        image: "/me.png",
+        twitterUsername: `@leggettc18`,
     },
-    plugins: ["gatsby-plugin-pnpm", "gatsby-plugin-image", "gatsby-plugin-postcss", {
-        resolve: 'gatsby-plugin-manifest',
-        options: {
-            icon: "src/images/icon.webp"
-        }
-    }, {
+    plugins: [
+        "gatsby-plugin-pnpm",
+        "gatsby-plugin-image",
+        "gatsby-plugin-postcss",
+        {
+            resolve: "gatsby-plugin-manifest",
+            options: {
+                icon: "src/images/icon.webp",
+            },
+        },
+        {
             resolve: `gatsby-plugin-mdx`,
             options: {
                 gatsbyRemarkPlugins: [
@@ -22,35 +27,45 @@ const config: GatsbyConfig = {
                         resolve: `gatsby-remark-images`,
                         options: {
                             maxWidth: 1200,
-                            backgroundColor: "transparent"
+                            backgroundColor: "transparent",
                         },
-                    }, "gatsby-remark-gifs", "gatsby-remark-prismjs"
+                    },
+                    "gatsby-remark-gifs",
+                    "gatsby-remark-prismjs",
                 ],
             },
-        }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-            resolve: 'gatsby-source-filesystem',
+        },
+        "gatsby-plugin-sharp",
+        "gatsby-transformer-sharp",
+        {
+            resolve: "gatsby-source-filesystem",
             options: {
                 name: "images",
-                path: "./src/images/"
+                path: "./src/images/",
             },
-        }, {
-            resolve: 'gatsby-source-filesystem',
+        },
+        {
+            resolve: "gatsby-source-filesystem",
             options: {
                 name: "portfolio",
-                path: path.resolve("content/portfolio")
+                path: path.resolve("content/portfolio"),
             },
-        }, {
-            resolve: 'gatsby-source-filesystem',
+        },
+        {
+            resolve: "gatsby-source-filesystem",
             options: {
                 name: "blog",
-                path: path.resolve("content/blog")
+                path: path.resolve("content/blog"),
             },
-        }, "gatsby-plugin-mdx-source-name", {
-            resolve: 'gatsby-plugin-layout',
+        },
+        "gatsby-plugin-mdx-source-name",
+        {
+            resolve: "gatsby-plugin-layout",
             options: {
-                component: path.resolve(`./src/components/layout.tsx`)
-            }
-        }]
+                component: path.resolve(`./src/components/layout.tsx`),
+            },
+        },
+    ],
 };
 
 export default config;
